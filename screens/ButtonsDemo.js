0f16f14a3bb042ad7611218568fc003419294e34
Josh Buchea <verctrl@joshbuchea.com>
2016-07-06 17:05:12 -0700

    [C] Update Buttons Demo

--- screens/ButtonsDemo.js
+++ screens/ButtonsDemo.js
@@ -28,83 +28,163 @@ export default function ButtonsDemo() {
   return (
     <Content>
       <View style={cs.padding}>
-        <View style={cs.p}><Button color="stable" text="Stable" /></View>
-        <View style={cs.p}><Button color="primary" text="Primary" /></View>
-        <View style={cs.p}><Button color="secondary" text="Secondary" /></View>
-        <View style={cs.p}><Button color="danger" text="Danger" /></View>
-        <View style={cs.p}><Button color="dark" text="Dark" /></View>
+        <View style={cs.p}>
+          <Button color="stable" text="Stable" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="primary" text="Primary" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="secondary" text="Secondary" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="danger" text="Danger" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="dark" text="Dark" onPress={() => {}} />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Outline</H3>
       <View style={cs.padding}>
-        <View style={cs.p}><Button color="stable" text="Stable Outline" outline /></View>
-        <View style={cs.p}><Button color="primary" text="Primary Outline" outline /></View>
-        <View style={cs.p}><Button color="secondary" text="Secondary Outline" outline /></View>
-        <View style={cs.p}><Button color="danger" text="Danger Outline" outline /></View>
-        <View style={cs.p}><Button color="dark" text="Dark Outline" outline /></View>
+        <View style={cs.p}>
+          <Button color="stable" text="Stable Outline" onPress={() => {}} outline />
+        </View>
+        <View style={cs.p}>
+          <Button color="primary" text="Primary Outline" onPress={() => {}} outline />
+        </View>
+        <View style={cs.p}>
+          <Button color="secondary" text="Secondary Outline" onPress={() => {}} outline />
+        </View>
+        <View style={cs.p}>
+          <Button color="danger" text="Danger Outline" onPress={() => {}} outline />
+        </View>
+        <View style={cs.p}>
+          <Button color="dark" text="Dark Outline" onPress={() => {}} outline />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Clear</H3>
       <View style={cs.padding}>
-        <View style={cs.p}><Button color="stable" text="Stable Clear" clear /></View>
-        <View style={cs.p}><Button color="primary" text="Primary Clear" clear /></View>
-        <View style={cs.p}><Button color="secondary" text="Secondary Clear" clear /></View>
-        <View style={cs.p}><Button color="danger" text="Danger Clear" clear /></View>
-        <View style={cs.p}><Button color="dark" text="Dark Clear" clear /></View>
+        <View style={cs.p}>
+          <Button color="stable" text="Stable Clear" onPress={() => {}} clear />
+        </View>
+        <View style={cs.p}>
+          <Button color="primary" text="Primary Clear" onPress={() => {}} clear />
+        </View>
+        <View style={cs.p}>
+          <Button color="secondary" text="Secondary Clear" onPress={() => {}} clear />
+        </View>
+        <View style={cs.p}>
+          <Button color="danger" text="Danger Clear" onPress={() => {}} clear />
+        </View>
+        <View style={cs.p}>
+          <Button color="dark" text="Dark Clear" onPress={() => {}} clear />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Round</H3>
       <View style={cs.padding}>
-        <View style={cs.p}><Button color="stable" text="Stable Round" round /></View>
-        <View style={cs.p}><Button color="primary" text="Primary Round" round /></View>
-        <View style={cs.p}><Button color="secondary" text="Secondary Round" round /></View>
-        <View style={cs.p}><Button color="danger" text="Danger Round" round /></View>
-        <View style={cs.p}><Button color="dark" text="Dark Round" round /></View>
+        <View style={cs.p}>
+          <Button color="stable" text="Stable Round" onPress={() => {}} round />
+        </View>
+        <View style={cs.p}>
+          <Button color="primary" text="Primary Round" onPress={() => {}} round />
+        </View>
+        <View style={cs.p}>
+          <Button color="secondary" text="Secondary Round" onPress={() => {}} round />
+        </View>
+        <View style={cs.p}>
+          <Button color="danger" text="Danger Round" onPress={() => {}} round />
+        </View>
+        <View style={cs.p}>
+          <Button color="dark" text="Dark Round" onPress={() => {}} round />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Block</H3>
       <View style={cs.padding}>
-        <View style={cs.marginBottom}><Button color="stable" text="Stable" /></View>
-        <View style={cs.marginBottom}><Button color="primary" text="Primary" /></View>
-        <View style={cs.marginBottom}><Button color="secondary" text="Secondary" /></View>
-        <View style={cs.marginBottom}><Button color="danger" text="Danger" /></View>
-        <View style={cs.marginBottom}><Button color="dark" text="Dark" /></View>
+        <View style={cs.marginBottom}>
+          <Button color="stable" text="Stable" onPress={() => {}} />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="primary" text="Primary" onPress={() => {}} />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="secondary" text="Secondary" onPress={() => {}} />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="danger" text="Danger" onPress={() => {}} />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="dark" text="Dark" onPress={() => {}} />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Full</H3>
       <View style={cs.paddingVertical}>
-        <View style={cs.marginBottom}><Button color="stable" text="Stable Full" full /></View>
-        <View style={cs.marginBottom}><Button color="primary" text="Primary Full" full /></View>
-        <View style={cs.marginBottom}><Button color="secondary" text="Secondary Full" full /></View>
-        <View style={cs.marginBottom}><Button color="danger" text="Danger Full" full /></View>
-        <View style={cs.marginBottom}><Button color="dark" text="Dark Full" full /></View>
+        <View style={cs.marginBottom}>
+          <Button color="stable" text="Stable Full" onPress={() => {}} full />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="primary" text="Primary Full" onPress={() => {}} full />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="secondary" text="Secondary Full" onPress={() => {}} full />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="danger" text="Danger Full" onPress={() => {}} full />
+        </View>
+        <View style={cs.marginBottom}>
+          <Button color="dark" text="Dark Full" onPress={() => {}} full />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Sizes</H3>
       <View style={cs.padding}>
-        <View style={cs.p}><Button color="stable" text="Stable Small" size="sm" /></View>
-        <View style={cs.p}><Button color="primary" text="Primary Small" size="sm" /></View>
-        <View style={cs.p}><Button color="secondary" text="Secondary Medium" /></View>
-        <View style={cs.p}><Button color="danger" text="Danger Medium" /></View>
-        <View style={cs.p}><Button color="dark" text="Dark Large" size="lg" /></View>
+        <View style={cs.p}>
+          <Button color="stable" text="Stable Small" size="sm" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="primary" text="Primary Small" size="sm" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="secondary" text="Secondary Medium" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="danger" text="Danger Medium" onPress={() => {}} />
+        </View>
+        <View style={cs.p}>
+          <Button color="dark" text="Dark Large" size="lg" onPress={() => {}} />
+        </View>
       </View>
 
       <H3 style={styles.sectionHeader}>Icons</H3>
       <View style={cs.padding}>
         <View style={cs.p}>
-          <Button color="stable"><Icon name="space-shuttle" size={22} /> Stable</Button>
+          <Button color="stable" onPress={() => {}}>
+            <Icon name="space-shuttle" size={22} /> Stable
+          </Button>
         </View>
         <View style={cs.p}>
-          <Button color="primary"><Icon name="home" size={22} /> Primary</Button>
+          <Button color="primary" onPress={() => {}}>
+            <Icon name="home" size={22} /> Primary
+          </Button>
         </View>
         <View style={cs.p}>
-          <Button color="secondary"><Icon name="users" size={22} /> Secondary</Button>
+          <Button color="secondary" onPress={() => {}}>
+            <Icon name="users" size={22} /> Secondary
+          </Button>
         </View>
         <View style={cs.p}>
-          <Button color="danger"><Icon name="pencil" size={22} /> Danger</Button>
+          <Button color="danger" onPress={() => {}}>
+            <Icon name="pencil" size={22} /> Danger
+          </Button>
         </View>
         <View style={cs.p}>
-          <Button color="dark"><Icon name="trash" size={22} /> Dark</Button>
+          <Button color="dark" onPress={() => {}}>
+            <Icon name="trash" size={22} /> Dark
+          </Button>
         </View>
       </View>
     </Content>
